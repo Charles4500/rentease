5c78fab50664292220884e78501a62cbaf59f1f4
CharlesKibet <charlesbiegon973@gmail.com>
2026-05-20 14:11:35 +0300

    chore(pref): rename file name

--- src/main/java/com/server/rentease/Application.java
+++ src/main/java/com/server/rentease/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RenteaseApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RenteaseApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
