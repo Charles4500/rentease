f4a2814586edc2c4a6cdfce0f350096704f9afdd
CharlesKibet <charlesbiegon973@gmail.com>
2026-05-20 14:02:42 +0300

    chore: project set up

---
+++ src/test/java/com/server/rentease/RenteaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.rentease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenteaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
